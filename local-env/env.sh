c66e70c843532f70f3bf2250826d8fe8e7f4a2be
Mindaugas Karpinskas <mkarpinskas@exadel.com>
2018-08-27 15:50:09 +0300

    utils

--- local-env/env.sh
+++
@@ -1,5 +0,0 @@
-
-
-sudo docker-compose up -d
-
-sudo bash -lic "sudo docker exec -i -t finalresult_redis_docker_1 /bin/bash"
---
+++ local-env/up.sh
@@ -0,0 +1,5 @@
+
+
+sudo docker-compose up -d
+
+# sudo bash -lic "sudo docker exec -i -t redis_docker /bin/bash"
